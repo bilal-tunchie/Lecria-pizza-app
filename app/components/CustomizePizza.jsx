20b6cc7136846cfb94e123d5042a164d1f7b7b4c
Bilal Mohamed <bilalmohammed_tunchie@outlook.com>
2023-06-09 18:10:59 +0300

    adding image to custom order

--- app/components/CustomizePizza.jsx
+++ app/components/CustomizePizza.jsx
@@ -150,7 +150,7 @@ export default function CustomizePizza() {
                 <h3>تعديل الطلب</h3>
             </div>
             <div className='customize'>
-                {customOrder.length && <PizzaCard customOrder={customOrder}  />}
+                {customOrder.img && <PizzaCard customOrder={customOrder}  />}
                 {customOrder.size && 
                 <SizeOptions 
                     customOrder={customOrder} 
